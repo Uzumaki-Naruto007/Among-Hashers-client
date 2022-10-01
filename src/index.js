d8a33d17e366a5f2e3b7dbb337853cbc6c249d84
Aditya <adperiwal@deloitte.com>
2022-10-01 11:08:35 +0530

    add node server

--- src/index.js
+++ src/index.js
@@ -57,8 +57,8 @@ class MyGame extends Phaser.Scene {
     super();
   }
 connectToServer(room, name){
-    socket = io(`localhost:3000?room=${room}&name=${name}`);
-  }
+  socket = io(`http://among-hashers-server.herokuapp.com?room=${room}&name=${name}`);
+}
 
 getRandomInt(max) {
     return Math.floor(Math.random() * max);
